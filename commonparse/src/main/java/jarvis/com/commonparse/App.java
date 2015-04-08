cc5bb3d1661c653b4c334ab5eddebc87d0884aaf
zjxbr <zjxbr@sohu.com>
2015-04-08 21:52:17 +0800

    1

---
+++ commonparse/src/main/java/jarvis/com/commonparse/App.java
@@ -0,0 +1,13 @@
+package jarvis.com.commonparse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
